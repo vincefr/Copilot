87bcdbeaf62758d3869e8de9fb225e6940094c30
vinc:) <vincent.barbotin@cewe.fr>
2023-05-31 08:17:41 +0100

    git reset HEAD~1

--- Combinaison html/script.js
+++ Combinaison html/script.js
@@ -1,13 +1,17 @@
-//fonction qui  genere un nombre aleatoire entre 1 et 5
-function getRandomNumber() {
-    return Math.floor(Math.random()*5) +1;
+//Fonction qui genere un nombre aleatoire entre 1 et 5
+window.onload = function() {function getRandomNumber(){
+    return Math.floor(Math.random()*5)+1;
 }
-//fonction qui affiche le nombre aleatoire dans la premiere cellule du tableau
+//Fonction qui affiche le nombre aleatoire  dans la premiere cellule du tableau
 function displayRandomNumber(){
+    console.log("La fonction displayRandomNumber a été appelée");
     var randomNumber = getRandomNumber();
-    document.getElementById("results").innerHTML = randomNumber;
+
+    document.getElementById("#resultat_generateOne").innerHTML=randomNumber;
+    document.getElementById("#resultat2_generateOne").innerHTML=randomNumber;
+
 }
-//Ajout d'un ecouteur d'evenement sur le bouton pour appeler la fonction  displayRandomNumber au clic
-window.onload = function() {
-    document.getElementById("random").addEventListener("click", displayRandomNumber);
+//Ajout d'un ecouteur d'evenement sur le bouton
+//pour appeler la fonction displayRandomNumber au clic
+document.getElementById("generateOne").addEventListener("click", displayRandomNumber);
 }
